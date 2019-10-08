323320ddb5367055523c96d9ff9352b2d9fc2997
Martin <martingeo15@gmail.com>
2019-10-08 14:26:28 +0530

    updated

--- django_1/blog/models.py
+++ django_1/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+'''Each class is a table
+Each variable declared inside
+is a field.
+
+Django uses Object-Relation Mapping
+to create an Object Orienetd Relational
+DB like experience.'''
+
+class Post(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	date_posted = models.DateTimeField(default=timezone.now)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.title
--- django_1/blog/views.py
+++ django_1/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-# dummy data
-posts = [
-	{
-		'Author' : 'Martin G.',
-		'Title' : 'Post 1',
-		'Content' : 'My Blog Post',
-		'Date' : 'October 7 2019',
-	},
-
-	{
-		'Author' : 'Elizabeth G.',
-		'Title' : 'Post 2',
-		'Content' : 'Her Blog Post',
-		'Date' : 'October 7 2019',
-	},
-]
 
 def home(request):
 	context = {
-		'posts' : posts
+		'title' : 'Blogs Here',
+		'posts' : Post.objects.all()
 	}
 
 	return render(request, 'blog/blog.html', context)
